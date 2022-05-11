736d3c067cd482338fa308594a2c4f7ef6c3ed33
Tim Vo <timtvo714@gmail.com>
2022-05-10 20:48:10 -0700

    preconnect link for google fonts

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,8 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true} />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap"
