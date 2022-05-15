a66245ee154b48228ed07f096dd23f221cb5be60
Tim Vo <timtvo714@gmail.com>
2022-05-15 15:30:23 -0700

    outlining

---
+++ pages/admin/[id].js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Sampler = () => {
+  return (
+    <div>
+      <div>Sampler Editor by ID</div>
+      <div>Sampler Editor Notes</div>
+      <div>auth protected</div>
+      <div>Client Side</div>
+      <div>Uses React Query</div>
+      <div>Scene Form</div>
+      <div>Similar to PlayerContainer</div>
+      <div>access via auth only links on home page</div>
+    </div>
+  );
+};
+
+export default Sampler;
---
+++ pages/admin/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  Stack,
+  Button,
+  Box,
+  Grid,
+  useMediaQuery,
+  Typography,
+  useTheme,
+  CircularProgress,
+} from '@mui/material';
+
+const Dashboard = () => {
+  return (
+    <Stack>
+      <Typography component={Box} variant="body2">
+        Dashboard page. WIP
+      </Typography>
+      <Typography component={Box} variant="body2">Things to add</Typography>
+      <Typography component={Box} variant="body2">Admin settings</Typography>
+      <Typography component={Box} variant="body2">Admin settings</Typography>
+      <Typography component={Box} variant="body2">Analytics??</Typography>
+
+    </Stack>
+  );
+};
+
+export default Dashboard;
