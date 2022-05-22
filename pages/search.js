8706ea76784e0241c558d4a9f6e1f5e296422f7d
Tim Vo <timtvo714@gmail.com>
2022-05-22 16:25:11 -0700

    styling search section

--- pages/search.js
+++ pages/search.js
@@ -1,14 +1,26 @@
-import {Typography} from '@mui/material';
-import {TextField, Box, Button, Grid, Stack} from '@mui/material';
+import {
+  Typography,
+  TextField,
+  Box,
+  Button,
+  Grid,
+  Stack,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
 import React, {useState} from 'react';
 import {useMutation} from 'react-query';
 import {searchScenes} from '../lib/api';
+import breakpoints from '../lib/helpers/breakpoints';
 
 const intitialState = {
   tricks: '',
 };
 
 const Search = () => {
+  const theme = useTheme ();
+  const mdUp = useMediaQuery (theme.breakpoints.up (breakpoints.medium));
+  const mdDown = useMediaQuery (theme.breakpoints.down (breakpoints.medium));
   const [state, setState] = useState (intitialState);
 
   const {mutateAsync, data} = useMutation (searchScenes, {});
@@ -35,57 +47,78 @@ const Search = () => {
   };
 
   return (
-    <Typography variant="h2">
-      Under Construction
-    </Typography>
+    // <Typography variant="h2">
+    //   Under Construction
+    // </Typography>
+    (
+      <Stack sx={{width: '100%'}} spacing={4}>
+        <Box>
+          <form onSubmit={submitHandler}>
+            <Stack spacing={2}>
+              <TextField
+                label="Trick Search"
+                variant="outlined"
+                size="medium"
+                fullWidth
+                name="tricks"
+                id="tricks"
+                onChange={handleChange}
+                value={state.tricks}
+              />
+              <Grid
+                container
+                direction="column"
+                justifyContent={'center'}
+                alignItems={'flex-end'}
+              >
+                <Button
+                  variant="contained"
+                  sx={{
+                    '&:hover': {
+                      backgroundColor: 'primary.light',
+                    },
+                  }}
+                  type="submit"
+                >
+                  Search
+                </Button>
+              </Grid>
+            </Stack>
+          </form>
+        </Box>
+        <Grid container direction={mdUp ? 'row' : 'column-reverse'}>
+          {/**player goes here */}
+          <Grid item sx={{px: '1rem', maxWidth: mdUp ? '35vw' : null}}>
+            <Typography>
+              Search Result(s):
+            </Typography>
+            <Grid container direction="column">
+              {data &&
+                data.map (item => (
+                  <Button
+                    size="small"
+                    key={item.id}
+                    fullWidth={false}
+                    sx={{
+                      textAlign: 'left',
+                      display: 'inline-block',
+                      width: 'auto',
+                    }}
+                  >
+                    {item.tricks}
+                  </Button>
+                ))}
+            </Grid>
+
+          </Grid>
+          <Grid item sx={{minHeight: '30vh', minWidth: '40vw'}}>
+            <Typography>Player</Typography>
+          </Grid>
+        </Grid>
+
+      </Stack>
+    )
   );
-  // <Stack sx={{width: '100%'}} spacing={4}>
-  //   <Box>
-  //     <form onSubmit={submitHandler}>
-  //       <Stack spacing={2}>
-  //         <TextField
-  //           label="Trick Search"
-  //           variant="outlined"
-  //           size="medium"
-  //           fullWidth
-  //           name="tricks"
-  //           id="tricks"
-  //           onChange={handleChange}
-  //           value={state.tricks}
-  //         />
-  //         <Grid
-  //           container
-  //           direction="column"
-  //           justifyContent={'center'}
-  //           alignItems={'flex-end'}
-  //         >
-  //           <Button
-  //             variant="contained"
-  //             sx={{
-  //               '&:hover': {
-  //                 backgroundColor: 'primary.light',
-  //               },
-  //             }}
-  //             type="submit"
-  //           >
-  //             Search
-  //           </Button>
-  //         </Grid>
-  //       </Stack>
-  //     </form>
-  //   </Box>
-  //   <Box sx={{px: '1rem'}}>
-  //     <Typography>
-  //       Search Result(s):
-  //     </Typography>
-  //     {data &&
-  //       data.map (item => (
-  //         <Typography key={item.id}>
-  //           {item.tricks}
-  //         </Typography>
-  //       ))}
-  //   </Box>
-  // </Stack>
 };
 
 //commit
