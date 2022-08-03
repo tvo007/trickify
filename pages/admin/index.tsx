0699cd2617f541967d0738f09f9a8bd58aa4e9ce
Tim Vo <timtvo714@gmail.com>
2022-08-02 21:12:52 -0700

    ts dependencies

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import {FC} from 'react';
 import {Stack, Box, Typography} from '@mui/material';
 
-const Dashboard = () => {
+const Dashboard: FC = () => {
   return (
     <Stack>
       <Typography component={Box} variant="body2">
