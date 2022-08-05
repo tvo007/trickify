b8e837d148e08b807a72eb3e4de6f37eab5de1b4
Tim Vo <timtvo714@gmail.com>
2022-08-04 21:37:29 -0700

    axios api interfaces

--- lib/api.ts
+++ lib/api.ts
@@ -3,40 +3,25 @@ import { ISampler, IScene } from "./interfaces";
 
 export const restAPI = process.env.NEXT_PUBLIC_REST_URL;
 
-type GetSamplersResponse = {
-  data: ISampler[];
-};
-
-type GetSingleSamplerResponse = {
-  data: ISampler;
-};
-
-type GetScenesResponse = {
-  data: IScene[];
-};
-
-type AddSceneResponse = {
-  data: IScene;
-};
 
 //rest api
 export const getSamplers = async () => {
-  const { data } = await axios.get<GetSamplersResponse>(`${restAPI}/samplers`);
+  const { data } = await axios.get<ISampler[]>(`${restAPI}/samplers`);
   return data;
 };
 
 export const getSamplerById = async (id) => {
-  const { data } = await axios.get<GetSingleSamplerResponse>(`${restAPI}/samplers/${id}`);
+  const { data } = await axios.get<ISampler>(`${restAPI}/samplers/${id}`);
   return data;
 };
 
 export const getScenesBySamplerId = async (id) => {
-  const { data } = await axios.get<GetScenesResponse>(`${restAPI}/scenes/sampler/${id}`);
+  const { data } = await axios.get<IScene[]>(`${restAPI}/scenes/sampler/${id}`);
   return data;
 };
 
 export const addScene = async (formData) => {
-  const { data } = await axios.post<AddSceneResponse>(`${restAPI}/scenes`, formData);
+  const { data } = await axios.post<IScene>(`${restAPI}/scenes`, formData);
   return data;
   //returns single obj response and not the whole array
   //id is sampler id
@@ -50,6 +35,6 @@ export const login = async (formData) => {
 
 //scene search
 export const searchScenes = async (formData) => {
-  const { data } = await axios.post<GetScenesResponse>(`${restAPI}/scenes/search`, formData);
+  const { data } = await axios.post<IScene[]>(`${restAPI}/scenes/search`, formData);
   return data;
 };
