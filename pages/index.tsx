4165665bf02a59e93c7e00fdc54417ce03a8896b
Tim Vo <timtvo714@gmail.com>
2022-11-14 18:57:49 -0800

    add slogan

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,28 @@
 import { GetStaticProps } from "next";
 import SamplerCard from "../components/SamplerCard";
 import { getSamplers } from "../lib/api";
-import { Stack, Typography } from "@mui/material";
+import { Box, Grid, Stack, Typography } from "@mui/material";
 
 export default function Home({ samplers }) {
   // if (!data) return <div>Loading...</div>;
 
   return (
-    <Stack direction="column" spacing={2}>
+    <Stack direction="column" spacing={4} sx={{ width: "100%" }}>
+      <Stack sx={{ pl: "8px" }}>
+        <Typography component={Box} variant="h5">
+          Watch Samplers. Learn Tricks.
+        </Typography>
+      </Stack>
       {!samplers && <h2>Loading...</h2>}
-      {samplers.length > 0 &&
-        samplers.map((sampler) => (
-          <SamplerCard key={sampler.id} sampler={sampler} />
-          // <div key={sampler.id}>{sampler.name}</div>
-        ))}
+      <Grid container rowSpacing={4} columns={{ xs: 1, sm: 2 }}>
+        {samplers.length > 0 &&
+          samplers.map((sampler) => (
+            <Grid item key={sampler.id} xs={1}>
+              <SamplerCard sampler={sampler} />
+            </Grid>
+            // <div key={sampler.id}>{sampler.name}</div>
+          ))}
+      </Grid>
 
       {samplers.length === 0 && (
         <Typography variant="h3">No samplers available.</Typography>
